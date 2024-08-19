bc38f279f88a1ea26bc107a14b318249b3464704
reys <reysluisa0@gmail.com>
2024-08-19 09:26:59 +0200

    up

--- ordermovisa2/lib/pages/constants.dart
+++ ordermovisa2/lib/pages/constants.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
-const String google_api_key = "AIzaSyA2VopmD5MsKUyO-YXEOPhPyApNA27ITrU";
+const String google_api_key = "";
 const Color primaryColor = Color(0xFF7B61FF);
 const double defaultPadding = 16.0;
